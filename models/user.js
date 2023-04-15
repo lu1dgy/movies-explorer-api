09b8992778b89a116d8587a49949b9458797a415
alex <lapkesa@gmail.com>
2023-04-15 14:09:43 +0300

    feat:create models for movie and user

---
+++ models/user.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (value) => validator.isEmail(value),
+      message: 'Неккоректный email адрес',
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+});
+
+const User = mongoose.model('user', userSchema);
+
+module.exports = User;
