9fa12ddb538c4add3f94634d71fb77216ae7e044
alex <lapkesa@gmail.com>
2023-05-09 17:41:14 +0300

    fix: change cors props

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,9 +1,9 @@
 const DEFAULT_ALLOWED_METHODS = 'GET,PUT,PATCH,POST,DELETE,HEAD';
 
 const allowedCors = [
-  'http://localhost:3001',
-  'localhost:3001',
-  'https://localhost:3001',
+  'http://localhost:3000',
+  'localhost:3000',
+  'https://localhost:3000',
   'http://movies.lapkes.nomoredomains.monster',
   'https://movies.lapkes.nomoredomains.monster',
   'movies.lapkes.nomoredomains.monster',
